95aca505e2321d5d23f4728a3f5e640798c2911b
Thomas Creagh <github@thomascreagh.mailer.me>
2026-03-18 13:35:11 +0000

    2026-03-18 13:34:47  26.05.20260213.a82ccc3

--- config/mastodon.nix
+++ config/mastodon.nix
@@ -6,11 +6,11 @@
     enableUnixSocket = false;
     webPort = 55001;
     localDomain = "social.thomascreagh.com";
-    configureNginx = false;
+    configureNginx = true;
     smtp.fromAddress = "noreply@social.thomascreagh.com";
     trustedProxy = "192.168.26.1";
     extraConfig = {
-      BIND = "0.0.0.0";
+      #BIND = "0.0.0.0";
       # SINGLE_USER_MODE = "true";
     };
     streamingProcesses = 2;
---
+++ config/nginx.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+
+    virtualHosts = {
+      "social.thomascreagh.com" = {
+        forceSSL = false;
+        enableACME = false;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:55001";
+          proxyWebsockets = true;
+        };
+        locations."/packs/" = {
+          root = "${pkgs.mastodon}/public";
+        };
+        locations."/assets/" = {
+          root = "${pkgs.mastodon}/public";
+        };
+        locations."/system/" = {
+          root = "${pkgs.mastodon}/public";
+        };
+      };
+    };
+  };
+}
