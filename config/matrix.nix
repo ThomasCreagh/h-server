f17e0a0c96e3cae7cc118c9147f41498111a2765
Thomas Creagh <github@thomascreagh.mailer.me>
2026-03-27 01:39:29 +0000

    2026-03-27 01:39:11  26.05.20260213.a82ccc3

--- config/matrix.nix
+++ config/matrix.nix
@@ -1,32 +1,46 @@
-{ config, pkgs, lib, ... }:
-
-let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   domain = "0x74.net";
   matrixDomain = "matrix.${domain}";
+  clientConfig = {
+    "m.homeserver".base_url = "https://${matrixDomain}";
+    "m.identity_server" = {};
+  };
+  serverConfig = {
+    "m.server" = "${matrixDomain}:443";
+  };
+  mkWellKnown = data: ''
+    default_type application/json;
+    add_header Access-Control-Allow-Origin *;
+    return 200 '${builtins.toJSON data}';
+  '';
 in {
   services.matrix-synapse = {
     enable = true;
     settings = {
       server_name = domain;
-      public.baseurl = "https://${matrixDomain}";
-   
- 
+      public_baseurl = "https://${matrixDomain}";
+
       listeners = [
         {
           port = 8008;
-          bind_addresses = [ "0.0.0.0" ];
+          bind_addresses = ["0.0.0.0"];
           type = "http";
           tls = false;
           x_forwarded = true;
           resources = [
             {
-              names = [ "client" "federation" ];
+              names = ["client" "federation"];
               compress = true;
             }
           ];
         }
       ];
-      
+
       database = {
         name = "psycopg2";
         allow_unsafe_locale = true;
@@ -36,25 +50,21 @@ in {
           host = "/run/postgresql";
         };
       };
-      
+
       max_upload_size_mib = 100;
       url_preview_enabled = true;
       enable_registration = false;
       enable_metrics = false;
       registration_shared_secret_path = "/var/lib/matrix-synapse/registration_secret";
+
       trusted_key_servers = [
         {
           server_name = "matrix.org";
         }
       ];
-
-      #federation_domain_whitelist = [];
-      #allow_public_rooms_over_federation = false;
-      #allow_public_rooms_without_auth = false;
-      #federation_sender_instances = [];
     };
   };
-  
+
   services.postgresql = {
     enable = true;
     ensureDatabases = ["matrix-synapse"];
